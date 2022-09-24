7ae5cc13334a3848b1388eb9163ac1930c630dfe
gustavonj <gustavo.nicolau+github@gmail.com>
2022-09-24 15:01:39 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,69 @@
+locals {
+  gcp_project      = "hack-aso-grupo-11"
+  region           = "us-central1"
+  credentials_path = "./credentials.json"
+  cloud_organization = "gustavonj"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.20"
+    }
+  }
+
+   cloud {
+     organization = "gustavonj"
+
+     workspaces {
+       name = "hack-aso-grupo-11"
+     }
+   }
+
+}
+
+provider "google" {
+  credentials = file(local.credentials_path)
+
+  project = local.gcp_project
+  region  = local.region
+}
+
+provider "google-beta" {
+  credentials = file(local.credentials_path)
+
+  project = local.gcp_project
+  region  = local.region
+}
+
+
+resource "google_artifact_registry_repository" "play-repo" {
+  location      = local.region
+  project       = local.gcp_project
+  repository_id = "spotmusicrepository"
+  description   = "Repository Docker for Play Music Application Images"
+  format        = "DOCKER"
+}
+
+resource "google_sql_database_instance" "spotmusic-db-main" {
+  name             = "spotmusic-db-main"
+  database_version = "MYSQL_8_0"
+  region           = "us-central1"
+
+  settings {
+    tier = "db-f1-micro"
+    disk_size = "10"
+  }
+}
+
+resource "google_sql_user" "users" {
+  name     = "spotuserdb"
+  instance = google_sql_database_instance.spotmusic-db-main.name
+  password = "hackg11"
+}
+
+resource "google_sql_database" "database" {
+  name     = "spotmusicdb"
+  instance = google_sql_database_instance.spotmusic-db-main.name
+}
